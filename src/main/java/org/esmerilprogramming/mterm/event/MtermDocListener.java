bb662a15c26f249e3bdc0a339275847bb13e06b3
eprogramming <00hf11@gmail.com>
2014-09-18 00:38:38 -0300

    cleanup document listener.

--- src/main/java/org/esmerilprogramming/mterm/event/MtermDocListener.java
+++ src/main/java/org/esmerilprogramming/mterm/event/MtermDocListener.java
@@ -15,9 +15,6 @@ package org.esmerilprogramming.mterm.event;
 
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
-
-import org.esmerilprogramming.mterm.Mterm;
 
 /**
  * Custom implementation of DocumentListener.
@@ -38,16 +35,6 @@ public class MtermDocListener implements DocumentListener {
 
   @Override
   public void removeUpdate(DocumentEvent e) {
-
-    String PS1 = Mterm.buildPS1();
-    try {
-      if (e.getDocument().getText(0,  PS1.length() + 1).equals(PS1)) {
-        
-      }
-    } catch (BadLocationException e1) {
-      // TODO Auto-generated catch block
-     
-    }
     
   }
 
