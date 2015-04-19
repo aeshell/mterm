4f438c2969086f2db1c29861c2b5a9f2a5dabf49
eprogramming <00hf11@gmail.com>
2015-04-19 20:58:52 -0300

    issue #105

--- src/main/groovy/org/jboss/aesh/mterm/action/BlockAction.groovy
+++ src/main/groovy/org/jboss/aesh/mterm/action/BlockAction.groovy
@@ -3,41 +3,38 @@
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the "License") you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
  * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.mterm.action;
+package org.jboss.aesh.mterm.action
 
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JTextArea;
+import javax.swing.AbstractAction
+import javax.swing.Action
+import javax.swing.JTextArea
 
-import org.jboss.aesh.mterm.util.MtermUtil;
+import org.jboss.aesh.mterm.util.MtermUtil
 
 /**
  * @author Helio Frota  00hf11 at gmail.com
  */
-@SuppressWarnings("serial")
-public class BlockAction extends AbstractAction {
+class BlockAction extends AbstractAction {
 
-    private Action action;
+  private final Action action
 
-    public BlockAction(Action action) {
-        this.action = action;
+  BlockAction(Action action) {
+    this.action = action
+  }
+
+  void actionPerformed(ActionEvent ae) {
+    JTextArea t = (JTextArea) ae.source
+    if (t.caretPosition > t.text.lastIndexOf(MtermUtil.instance.ps1) + MtermUtil.instance.ps1.length()) {
+      action.actionPerformed(null)
     }
-
-    public void actionPerformed(ActionEvent ae) {
-
-        JTextArea t = (JTextArea) ae.getSource();
-        if (t.getCaretPosition() > t.getText().lastIndexOf(MtermUtil.instance.ps1) + MtermUtil.instancel.ps1.length()) {
-            action.actionPerformed(null);
-        }
-
-    }
+  }
 }
