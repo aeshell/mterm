8d5d360af952f5fe69daed1f90ef587ff66be7f9
eprogramming <00hf11@gmail.com>
2015-04-19 23:00:24 -0300

    issue #105

--- src/main/groovy/org/jboss/aesh/mterm/action/TabAction.groovy
+++ src/main/groovy/org/jboss/aesh/mterm/action/TabAction.groovy
@@ -3,45 +3,42 @@
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the 'License') you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
- * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.mterm.action;
+package org.jboss.aesh.mterm.action
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-
-import javax.swing.JTextArea;
-
-import org.jboss.aesh.mterm.util.MtermUtil;
+import java.awt.event.ActionEvent
+import javax.swing.JTextArea
+import org.jboss.aesh.mterm.util.MtermUtil
 
 /**
  * @author Helio Frota  00hf11 at gmail.com
  */
-@SuppressWarnings("serial")
-public class TabAction extends BaseAction {
+class TabAction extends BaseAction {
 
-    public TabAction(JTextArea textArea) {
-        super(textArea);
-    }
+  TabAction(JTextArea textArea) {
+    super(textArea)
+  }
 
-    public void actionPerformed(ActionEvent ev) {
-        perform();
-    }
+  @Override
+  void actionPerformed(ActionEvent ev) {
+    perform()
+  }
 
-    protected void perform() {
-        try {
-            aesh.run("\t");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.print(aesh.getResult() + MtermUtil.instance.ps1)
-        aesh.reset()
+  @Override
+  protected void perform() {
+    try {
+      aesh.run('\t')
+    } catch (IOException e) {
+      e.printStackTrace()
     }
+    System.out.print(aesh.getResult() + MtermUtil.instance.ps1)
+    aesh.reset()
+  }
 
 }
