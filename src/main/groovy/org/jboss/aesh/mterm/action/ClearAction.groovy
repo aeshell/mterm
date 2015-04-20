21da6b5acf95ca9aca8950a3ddc7f1b4ba584046
eprogramming <00hf11@gmail.com>
2015-04-19 21:05:17 -0300

    issue #105

--- src/main/groovy/org/jboss/aesh/mterm/action/ClearAction.groovy
+++ src/main/groovy/org/jboss/aesh/mterm/action/ClearAction.groovy
@@ -3,43 +3,43 @@
  * other contributors as indicated by the @authors tag. All rights reserved. See the copyright.txt
  * in the distribution for a full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the "License") you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
  * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.jboss.aesh.mterm.action;
+package org.jboss.aesh.mterm.action
 
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent
 
-import javax.swing.JTextArea;
-import javax.swing.text.BadLocationException;
+import javax.swing.JTextArea
+import javax.swing.text.BadLocationException
 
-import org.jboss.aesh.mterm.util.MtermUtil;
+import org.jboss.aesh.mterm.util.MtermUtil
 
 /**
  * @author Helio Frota  00hf11 at gmail.com
  */
-@SuppressWarnings("serial")
-public class ClearAction extends BaseAction {
+class ClearAction extends BaseAction {
 
-    public ClearAction(JTextArea textArea) {
-        super(textArea);
+  ClearAction(JTextArea textArea) {
+    super(textArea)
+  }
+
+  @Override
+  void actionPerformed(ActionEvent ae) {
+    try {
+      textArea.document.remove(0, textArea.document.length)
+    } catch (BadLocationException e) {
+      e.printStackTrace()
     }
+    System.out.print(MtermUtil.instance.ps1)
+  }
 
-    public void actionPerformed(ActionEvent ae) {
-        try {
-            textArea.getDocument().remove(0, textArea.getDocument().getLength());
-        }
-        catch (BadLocationException e) {
-            e.printStackTrace();
-        }
-        System.out.print(MtermUtil.instance.ps1)
-    }
+  @Override
+  protected void perform() {
 
-    protected void perform() {
-
-    }
+  }
 }
